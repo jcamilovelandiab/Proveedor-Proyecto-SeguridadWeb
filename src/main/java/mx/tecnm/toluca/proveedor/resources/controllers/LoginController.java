f4adbf74f029105a743f21f4903f4336f862c195
jcamilovelandiab <jcamilovelandiab>
2020-03-31 22:50:54 -0500

    dummb changes

---
+++ src/main/java/mx/tecnm/toluca/proveedor/resources/controllers/LoginController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.tecnm.toluca.proveedor.resources.controllers;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author jcami
+ */
+@Path("login")
+public class LoginController {
+    
+    @POST
+    public String login(){
+        return null;
+    }
+    
+}
